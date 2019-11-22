38b74d94ee7f0956cb4e4b2b11a3a632067e512e
Andrew Hyslop CSM2017 <andrew.hyslop.2017@uni.strath.ac.uk>
2019-11-22 16:29:03 +0000

    Add new file

---
+++ src/templates/signup.php
@@ -0,0 +1,76 @@
+<?php include "header.php" ?>
+
+  <!-- Header -->
+  <header class="masthead text-white text-center">
+    <div class="overlay"></div>
+    <div class="container">
+      <div class="row">
+        <div class="col-xl-9 mx-auto">
+          <h1 class="mb-5">Get started by signing up!</h1>
+        </div>
+        <div class="col-md-10 col-lg-8 col-xl-7 mx-auto">
+          <form>
+            <div class="form-row">
+              <div class="col-12 col-md-9 mb-2 mb-md-0">
+                <input type="email" class="form-control form-control-lg" placeholder="Enter your email...">
+              </div>
+              <div class="col-12 col-md-3">
+                <button type="submit" class="btn btn-block btn-lg btn-primary" src = "signup.php">Sign up!</button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </header>
+
+  <!-- Here's  what our app can do for you...  -->
+  <section class="features-icons bg-light text-center">
+    <div class="container">
+      <h1>Fill out this form below to sign up to use our site</h1>
+        </div>
+  </section>
+
+  <!-- Some stuff about the environment-->
+  <section class="showcase">
+    <div class="container-fluid p-0">
+      <div class="row no-gutters">
+
+        <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('../img/bg-showcase-1.jpg');"></div>
+        <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+          <h2>Source Local Food</h2>
+          <p class="lead mb-0">By using our app you will be supporting local farmers and businesses in your area.
+		                       The app will use google Earth Api to source places for you to shop locally and not 
+		                       only help the enviroment, but also support local businesses</p>
+		                    
+        </div>
+      </div>
+      <div class="row no-gutters">
+        <div class="col-lg-6 text-white showcase-img" style="background-image: url('../img/bg-showcase-2.jpg')"></div>
+        <div class="col-lg-6 my-auto showcase-text">
+          <h2>Recommendations from other users</h2>
+          <p class="lead mb-0">.......................</p>
+        </div>
+      </div>
+      <div class="row no-gutters">
+        <div class="col-lg-6 order-lg-2 text-white showcase-img" style="background-image: url('../img/bg-showcase-3.jpg');"></div>
+        <div class="col-lg-6 order-lg-1 my-auto showcase-text">
+          <h2>Reduce Carbon footprint</h2>
+          <p class="lead mb-0">By using our site, you are activley reducing your carbon footprint. By locallly sourcing whereto buy your food, you are cutting down emissions produced by shopping typically in Tescos, Morrisons, and Asda. You are also helping a local business and in turn giving back to your community.</p>
+        </div>
+      </div>
+    </div>
+  </section>
+  
+  
+    <!-- Click here to try it now...  -->
+  <section class="features-icons bg-light text-center">
+    <div class="container">
+     <h1> <a href="#"> Try It Now! </a></h1>
+        </div>
+
+  </section>
+
+ 
+
+<?php include "footer.php" ?>
